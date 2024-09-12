428ea88a047a8068d3a6b3eac436ec469564fd03
Christopher <christopherd1682@gmail.com>
2024-09-12 12:16:30 -0700

    SOPHI-2: Code coverage

--- src/test/java/com/example/springbootgithubactiondemo/OneBasedServiceApplicationTests.java
+++ src/test/java/com/example/springbootgithubactiondemo/OneBasedServiceApplicationTests.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SpringBootGithubActionDemoApplicationTests {
+class OneBasedServiceApplicationTests {
 
     @Autowired
     private HomeController controller;
@@ -25,6 +25,7 @@ class SpringBootGithubActionDemoApplicationTests {
     @Test
     void contextLoads() {
     }
+
 
     @Test
     void getHomeController() throws Exception {
